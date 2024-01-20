3dcc1482e5903d857bc9e25cd8e6b08a249f41a2
Bozhidar Nemkski <bojonemski@gmail.com>
2024-01-20 16:06:35 +0200

    style(dailytasks): style DailyTasks screen

--- src/screens/DailyTasks.tsx
+++ src/screens/DailyTasks.tsx
@@ -1,16 +1,53 @@
 import { useLayoutEffect } from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
+import { Task } from '@/components/Task';
+import { IconButton } from '@/components/common';
+import { COLORS, ICON_GROUPS } from '@/constants';
 import { DailyTasksProps } from '@/types/navigator-types/root-stack-param-list';
 
 export default function DailyTasks({ route, navigation }: DailyTasksProps) {
   useLayoutEffect(() => {
-    navigation.setOptions({ title: route.params?.day });
+    navigation.setOptions({
+      title: route.params?.day,
+      headerRight: () => <Text style={styles.headerRightStyles}>98%</Text>,
+    });
   }, [navigation, route.params?.day]);
 
   return (
-    <View>
-      <Text>Currently there are not tasks</Text>
-    </View>
+    <>
+      <ScrollView>
+        {Array(20)
+          .fill(0)
+          .map((_, index) => (
+            <Task key={index} />
+          ))}
+      </ScrollView>
+      <View style={styles.addButtonContainer}>
+        <IconButton
+          iconGroup={ICON_GROUPS.Ionicons}
+          icon="add-circle"
+          color={COLORS.SECONDARY_300}
+          size={56}
+          onPress={() => {}}
+          stylesOnPressed={styles.addButtonPressed}
+        />
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  headerRightStyles: {
+    color: COLORS.PRIMARY,
+    fontSize: 16,
+  },
+  addButtonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+  },
+  addButtonPressed: {
+    opacity: 0.75,
+  },
+});
