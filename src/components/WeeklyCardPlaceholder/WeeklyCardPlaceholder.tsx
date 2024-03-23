5d1364b465e72526a569d8ea2ed064e8af3e45e9
Bozhidar Nemkski <bojonemski@gmail.com>
2024-03-23 17:56:51 +0200

    style(weekly-card-placeholder): style the shimmer placeholders shown while weeks are loading

--- src/components/WeeklyCardPlaceholder/WeeklyCardPlaceholder.tsx
+++ src/components/WeeklyCardPlaceholder/WeeklyCardPlaceholder.tsx
@@ -1,14 +1,44 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import { Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import ShimmerPlaceHolder from 'react-native-shimmer-placeholder';
 
 export default function WeeklyCardPlaceholder() {
-  return (
-    <>
-      <ShimmerPlaceHolder LinearGradient={LinearGradient} />
-      <ShimmerPlaceHolder visible LinearGradient={LinearGradient}>
-        <Text>Wow, awesome here.</Text>
-      </ShimmerPlaceHolder>
-    </>
-  );
+  const placeholders = [0, 1, 2];
+  return placeholders.map((id) => (
+    <View style={styles.ShimmerPlaceholderWrappe} key={id}>
+      <ShimmerPlaceHolder
+        style={[styles.shimmerPlaceHolder]}
+        LinearGradient={LinearGradient}
+      />
+      <ShimmerPlaceHolder
+        style={[styles.shimmerPlaceHolder, styles.shimmerPlaceHolderSubtitle]}
+        LinearGradient={LinearGradient}
+      />
+      <ShimmerPlaceHolder
+        style={[
+          styles.shimmerPlaceHolder,
+          styles.shimmerPlaceholderProgressBar,
+        ]}
+        LinearGradient={LinearGradient}
+      />
+    </View>
+  ));
 }
+
+const styles = StyleSheet.create({
+  ShimmerPlaceholderWrappe: {
+    marginVertical: 10,
+    marginHorizontal: 26,
+    rowGap: 12,
+  },
+  shimmerPlaceHolder: {
+    borderRadius: 10,
+  },
+  shimmerPlaceHolderSubtitle: {
+    height: 10,
+  },
+  shimmerPlaceholderProgressBar: {
+    width: '100%',
+    height: 12,
+  },
+});
