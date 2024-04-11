0dfde009512b542038be8af27a92b4b4177dfe13
Bozhidar Nemkski <bojonemski@gmail.com>
2024-04-11 08:09:40 +0300

    feat(custom-button): make CustomButton render an icon as an option

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -7,13 +7,22 @@ import {
   ViewStyle,
 } from 'react-native';
 
+import Icon from './Icon';
+
 import { COLORS } from '@/constants';
+import { IconTypes } from '@/types';
 
 interface CustomButtonProps {
   text: string;
   buttonStyles?: StyleProp<ViewStyle>;
   textStyles?: StyleProp<ViewStyle>;
   pressedStyles?: StyleProp<ViewStyle>;
+  icon?: {
+    iconGroup: string;
+    icon: IconTypes;
+    color: string;
+    size: number;
+  };
   onPress: () => void;
 }
 
@@ -22,6 +31,7 @@ export default function CustomButton({
   buttonStyles,
   textStyles,
   pressedStyles,
+  icon,
   onPress,
 }: CustomButtonProps) {
   return (
@@ -33,6 +43,14 @@ export default function CustomButton({
       ]}
       onPress={onPress}
     >
+      {icon && (
+        <Icon
+          iconGroup={icon.iconGroup}
+          icon={icon.icon}
+          color={COLORS.PRIMARY}
+          size={24}
+        />
+      )}
       <Text style={[styles.text, textStyles]}>{text}</Text>
     </Pressable>
   );
@@ -40,12 +58,15 @@ export default function CustomButton({
 
 const styles = StyleSheet.create({
   root: {
+    flexDirection: 'row',
     width: '60%',
     height: 50,
     borderWidth: 1,
     borderRadius: 30,
     alignSelf: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    columnGap: 10,
     backgroundColor: COLORS.SECONDARY_200,
     elevation: 4,
   },
