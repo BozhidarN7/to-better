4d8b2da48e72f9374de08af38c7bb83073f7c85e
Bozhidar Nemski <bojonemski@gmail.com>
2024-04-14 16:35:08 +0300

    feat(calendar-modal): create simple calendar modal window

--- src/components/CalendarButton/CalendarButton.tsx
+++ src/components/CalendarButton/CalendarButton.tsx
@@ -1,25 +1,50 @@
-import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import {
+  Modal,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 
 import { CustomButton } from '../common';
 
 import { COLORS, ICON_GROUPS } from '@/constants';
 
 export default function CalendarButton() {
+  const [shouldShowCalendarModal, setShouldShowCalendarModal] = useState(false);
+  console.log('to-better:shouldShowModal', shouldShowCalendarModal);
   return (
-    <View style={styles.weeksButtonContainer}>
-      <CustomButton
-        text="Weeks"
-        buttonStyles={styles.weeksButton}
-        pressedStyles={styles.weeksButtonPressed}
-        onPress={() => {}}
-        icon={{
-          iconGroup: ICON_GROUPS.Ionicons,
-          icon: 'calendar',
-          color: COLORS.PRIMARY,
-          size: 24,
-        }}
-      />
-    </View>
+    <>
+      <View style={styles.weeksButtonContainer}>
+        <CustomButton
+          text="Weeks"
+          buttonStyles={styles.weeksButton}
+          pressedStyles={styles.weeksButtonPressed}
+          onPress={() => {
+            console.log('hre');
+            setShouldShowCalendarModal((prev) => !prev);
+          }}
+          icon={{
+            iconGroup: ICON_GROUPS.Ionicons,
+            icon: 'calendar',
+            color: COLORS.PRIMARY,
+            size: 24,
+          }}
+        />
+      </View>
+      <Modal animationType="fade" transparent visible={shouldShowCalendarModal}>
+        <TouchableWithoutFeedback
+          onPress={() => setShouldShowCalendarModal(false)}
+        >
+          <View style={styles.calendarModalContainer}>
+            <View style={styles.calendarModalContent}>
+              <Text>This is a modal window</Text>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+    </>
   );
 }
 
@@ -35,5 +60,25 @@ const styles = StyleSheet.create({
   },
   weeksButtonPressed: {
     opacity: 0.75,
+  },
+  calendarModalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  calendarModalContent: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
   },
 });
