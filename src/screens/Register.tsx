7c3592cd7720330e66131940e6572dbec0de2b08
Bozhidar Nemski <bojonemski@gmail.com>
2024-07-30 12:02:01 +0300

    style(register-screen): add icons for the text input fields

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,41 +1,66 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-import { COLORS } from '@/constants';
+import { Icon } from '@/components/common';
+import { COLORS, ICON_GROUPS } from '@/constants';
 
 export default function Register() {
   return (
     <View style={styles.registerContainer}>
       <Text style={styles.primaryHeader}>Create account</Text>
       <Text style={styles.secondaryHeader}>Please enter your details</Text>
-      <View style={styles.inputFieldContainer}>
+      <View style={styles.inputGroupContainer}>
         <Text style={styles.inputLabel} aria-label="Email">
           Your email
         </Text>
-        <TextInput
-          style={styles.inputField}
-          aria-label="input"
-          placeholder="Enter your email"
-        />
+        <View style={styles.inputFieldContainer}>
+          <TextInput
+            style={styles.inputField}
+            aria-label="input"
+            placeholder="Enter your email"
+          />
+          <Icon
+            iconGroup={ICON_GROUPS.FontAwesome}
+            icon="envelope-o"
+            color={COLORS.BLACK}
+            size={22}
+          />
+        </View>
       </View>
-      <View style={styles.inputFieldContainer}>
+      <View style={styles.inputGroupContainer}>
         <Text style={styles.inputLabel} aria-label="Password">
           Password
         </Text>
-        <TextInput
-          style={styles.inputField}
-          aria-label="input"
-          placeholder="Enter your password"
-        />
+        <View style={styles.inputFieldContainer}>
+          <TextInput
+            style={styles.inputField}
+            aria-label="input"
+            placeholder="Enter your password"
+          />
+          <Icon
+            iconGroup={ICON_GROUPS.Ionicons}
+            icon={true ? 'eye-outline' : 'eye-off-outline'}
+            color={COLORS.BLACK}
+            size={24}
+          />
+        </View>
       </View>
-      <View style={styles.inputFieldContainer}>
+      <View style={styles.inputGroupContainer}>
         <Text style={styles.inputLabel} aria-label="Repeat password">
           Repeat password
         </Text>
-        <TextInput
-          style={styles.inputField}
-          aria-label="input"
-          placeholder="Repeat password"
-        />
+        <View style={styles.inputFieldContainer}>
+          <TextInput
+            style={styles.inputField}
+            aria-label="input"
+            placeholder="Repeat password"
+          />
+          <Icon
+            iconGroup={ICON_GROUPS.Ionicons}
+            icon={false ? 'eye-outline' : 'eye-off-outline'}
+            color={COLORS.BLACK}
+            size={24}
+          />
+        </View>
       </View>
     </View>
   );
@@ -55,16 +80,22 @@ const styles = StyleSheet.create({
     fontSize: 15,
     marginBottom: 20,
   },
-  inputFieldContainer: {
+  inputGroupContainer: {
     marginBottom: 15,
+  },
+  inputFieldContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: COLORS.PRIMARY_100,
+    borderRadius: 6,
+    paddingRight: 15,
   },
   inputLabel: {
     marginBottom: 8,
   },
   inputField: {
-    backgroundColor: COLORS.PRIMARY_100,
+    flex: 1,
     height: 40,
-    borderRadius: 6,
     paddingLeft: 15,
   },
 });
